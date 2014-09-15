80ef882fccd5054badae259a02da8a3c08ae23f7
Jeff Risberg <jrisberg@aol.com>
2014-09-15 14:44:34 -0700

    integrated pricing information into shopping cart

--- controllers.js
+++ controllers.js
@@ -2,6 +2,8 @@ function HomeController($scope, $filter) {
 
     $scope.appState = "home";
 
+    $scope.discount = 0.0;
+
     $scope.items = [
         {title: 'Paint pots', quantity: 8, price: 3.95},
         {title: 'Polka dots', quantity: 17, price: 12.95},
@@ -19,4 +21,22 @@ function HomeController($scope, $filter) {
 
         $scope.appState = "home";
     };
+
+    $scope.totalCart = function () {
+        var total = 0;
+        for (var i = 0, len = $scope.items.length; i < len; i++) {
+            total = total + $scope.items[i].price * $scope.items[i].quantity;
+        }
+        return total;
+    }
+
+    $scope.subtotal = function () {
+        return $scope.totalCart() - $scope.discount;
+    };
+
+    function calculateDiscount(newValue, oldValue, scope) {
+        $scope.discount = newValue > 100 ? 10 : 0;
+    }
+
+    $scope.$watch($scope.totalCart, calculateDiscount);
 }
